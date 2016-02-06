705ea7431577c57ba3cbcad16fa0facd44617803
Thoshh <thumbimigwe@gmail.comgit config --global user.name thumbimigwe>
2016-02-06 13:46:14 +0300

    ex1

---
+++ js/app.js
@@ -0,0 +1,28 @@
+    $(document).foundation();
+    //
+    // var team = prompt("So, What's your favourite team?")
+    // if (team != null) {
+    //   console.log(team + " - This is a poor team")
+    // }
+
+
+    // EXERCISE 2
+
+    var hour, minute;
+    hour = 1;
+    minute = 40;
+
+    var remHour = 24 - hour;
+    var remMinute = (60 - minute) + (remHour * 60);
+    var remSec = remMinute * 60;
+    console.log(remSec);
+
+
+
+    // alert(
+    //   "This site is extremely awesome and should thus be enjoyed with caution."
+    // )
+    // var name = prompt("What's your name?")
+    // if (name != null) {
+    //   document.write("welcome " + name);
+    // }
