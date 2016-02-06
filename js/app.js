2f7782925834da2b844f981acb6df53a9b6bd605
thumbimigwe <thumbimigwe@gmail.com>
2016-02-06 14:52:51 +0300

    ex3 (b)

--- js/app.js
+++ js/app.js
@@ -20,17 +20,33 @@
 
     // EXERCISE 3
 
-    //
-    var name = "Thoshh";
-    var age = 24;
-    var sex = "male";
-    var favFood = "Fruits";
-    var occupation = "student";
-    var school = "Moringa school";
-    var physicalLocation = "Galana Plaza";
-    // var bestFriend
-    // var birthday
-    // var language
+    // var name = "Thoshh";
+    // var age = 24;
+    // var sex = "male";
+    // var favFood = "Fruits";
+    // var occupation = "student";
+    // var school = "Moringa school";
+    // var physicalLocation = "Galana Plaza";
+    // // var bestFriend
+    // // var birthday
+    // // var language
+    // console.log("Hi, this is " + name + ". I am a " + age + " year old " + sex +
+    //   ". I am a " + occupation + " at " + school + " which is at " +
+    //   physicalLocation + ". I like " + favFood + " as my favourite Food.");
+
+
+    // EXERCISE 3 PROMPT
+
+    var name = prompt("What is your name?");
+    var age = prompt("How old are you?");
+    var sex = prompt("Sex?");
+    var favFood = prompt("What is your favourite food?");
+    var occupation = prompt("What's your occupation?");
+    var school = prompt("Which school do you go to?");
+    var physicalLocation = prompt("Where is the school?");
+    // // var bestFriend
+    // // var birthday
+    // // var language
     console.log("Hi, this is " + name + ". I am a " + age + " year old " + sex +
       ". I am a " + occupation + " at " + school + " which is at " +
       physicalLocation + ". I like " + favFood + " as my favourite Food.");
